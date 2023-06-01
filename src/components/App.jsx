57fbaa1aa882b49eedf19771ce98b9414d7409bd
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2023-06-01 16:54:45 +0300

    Update App.jsx
    useState hook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,42 +1,40 @@
-import React, { Component } from 'react';
+import { useState } from "react";
 import Statistics from './Statistics/Statistics';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Section from './Section/Section';
 import Notification from './Notification/Notification';
 import { AppContainer } from './App.styled';
 
-class App extends Component {
+const App = () => {
 
-state = {
-      good: 0,
-      neutral: 0,
-      bad: 0
-    };
+const [state, setState] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0
+  });
 
-
-  handleButtonClick = (type) => {
-    this.setState((prevState) => ({
+  const handleButtonClick = (type) => {
+    setState(prevState => ({
       ...prevState,
       [type]: prevState[type] + 1
     }));
   };
 
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
+  const countTotalFeedback = () => {
+    const { good, neutral, bad } = state;
     return good + neutral + bad;
   };
 
-  countPositiveFeedbackPercentage = () => {
-    const { good } = this.state;
-    const total = this.countTotalFeedback();
+const countPositiveFeedbackPercentage = () => {
+    const { good } = state;
+    const total = countTotalFeedback();
     return total === 0 ? 0 : Math.round((good / total) * 100);
   };
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    const totalFeedback = this.countTotalFeedback();
-    const positiveFeedbackPercentage = this.countPositiveFeedbackPercentage();
-    let feedbackContent;
+  const { good, neutral, bad } = state;
+  const totalFeedback = countTotalFeedback();
+  const positiveFeedbackPercentage = countPositiveFeedbackPercentage();
+  let feedbackContent;
 
 if (totalFeedback === 0) {
   feedbackContent = <Notification message="There is no feedback" />;
@@ -57,15 +55,12 @@ return (
     <Section title="Review Widget 📝">
       <FeedbackOptions
         options={['good', 'neutral', 'bad']}
-        onLeaveFeedback={this.handleButtonClick}
+        onLeaveFeedback={handleButtonClick}
       />
     </Section>
-
     <Section title="Statistics 📊">{feedbackContent}</Section>
   </AppContainer>
-);
-
-  }
+  );
 }
 
 export default App;
